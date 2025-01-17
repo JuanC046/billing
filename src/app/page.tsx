8d911ce05d7042fe34d30394a09535df6787be0a
Juan <juan.lopez15@utp.edu.co>
2025-01-17 17:35:42 -0500

    Refactor authentication logic to use async/await and implement token storage in localStorage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+import { auth } from "@/services/auth";
+import { useEffect } from "react";
 export default function Home() {
-  return (
-    <>
-    </>
-  );
+    useEffect(() => {
+        const getTokens = async () => {
+            try {
+                const data = await auth();
+                console.log(data);
+                localStorage.setItem("access_token", data.access_token);
+                localStorage.setItem("refresh_token", data.refresh_token);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        getTokens();
+    }, []);
+    return <></>;
 }
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,33 +1,25 @@
 import qs from "qs";
-export const auth = () => {
-    fetch(`${process.env.NEXT_PUBLIC_URL_API}/oauth/token`, {
-        method: "POST",
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: qs.stringify({
-            grant_type: "password",
-            client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
-            client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
-            username: process.env.NEXT_PUBLIC_EMAIL,
-            password: process.env.NEXT_PUBLIC_PASSWORD,
-        }),
-    })
-        .then((response) => {
-            console.log(response);
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error("Network response was not ok.");
-        })
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((error) => {
-            console.error(
-                "There has been a problem with your fetch operation:",
-                error
-            );
-        });
+export const auth = async () => {
+    const response = await fetch(
+        `${process.env.NEXT_PUBLIC_URL_API}/oauth/token`,
+        {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: qs.stringify({
+                grant_type: "password",
+                client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+                client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+                username: process.env.NEXT_PUBLIC_EMAIL,
+                password: process.env.NEXT_PUBLIC_PASSWORD,
+            }),
+        }
+    );
+    if (response.ok) {
+        const data = await response.json();
+        return data;
+    }
+    throw new Error("Failed to authenticate");
 };
